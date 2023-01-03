f217bc7a1e8d6f7a614a371b7f03122c018afba9
André A. Gomes <andremegafone@gmail.com>
2023-01-03 18:41:05 +0200

    keyboards/aadcg_avalanche: Add config.

---
+++ keyboards/aadcg_avalanche/rules.mk
@@ -0,0 +1,12 @@
+MCU		        = atmega32u4
+BOOTLOADER      = caterina
+OLED_DRIVER     = SSD1306
+OLED_ENABLE     = yes
+SPLIT_KEYBOARD  = yes
+ENCODER_ENABLE  = yes
+CONSOLE_ENABLE  = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+MUSIC_ENABLE    = no
+# link time optimization, which results in a smaller compiled binary
+LTO_ENABLE      = yes
